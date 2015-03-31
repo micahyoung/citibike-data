66c882b0d759b63d9f61960d5e2cd36a90ebaa4c
Micah Young <micah@young.io>
2015-03-31 13:13:46 +0000

    Update script url to new 302

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -4,7 +4,7 @@ JSONDIR=`dirname $0`/../json
 FILE=`date +%s`.json
 
 cd $JSONDIR
-curl --retry 30 http://appservices.citibikenyc.com/data2/stations.php > $FILE
+curl --retry 30 http://appservices.citibikenyc.com/v1/station/list > $FILE
 git add -A .
 git commit -m`date +%s`
 test -n "$1" && git push
