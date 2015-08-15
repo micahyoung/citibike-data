7d4e068efe81aae8d447deb9af92ce0dc423044c
Micah Young <micah@young.io>
2015-08-15 21:50:17 +0000

    Generate index with each fetch

--- bin/fetch.sh
+++ bin/fetch.sh
@@ -5,6 +5,8 @@ FILE=`date +%s`.json
 
 cd $JSONDIR
 curl --retry 30 http://appservices.citibikenyc.com/v1/station/list > $FILE
+ls *.json > ../index.txt
+cd ../
 git add -A .
 git commit -m`date +%s`
 test -n "$1" && git push
