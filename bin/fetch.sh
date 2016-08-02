38cf73f65cd07ae04520750666dded9d4249c4c1
Micah Young <micah@young.io>
2016-08-02 00:04:40 -0400

    Ophan

---
+++ bin/fetch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+JSONDIR=`dirname $0`/../json
+FILE=`date +%s`.json
+
+cd $JSONDIR
+curl --retry 30 http://appservices.citibikenyc.com/v1/station/list > $FILE
+ls -r | sed 's/\.json$//' > ../timestamps.txt
+cd ../
+git add -A .
+git commit -m`date +%s`
+test -n "$1" && git push
